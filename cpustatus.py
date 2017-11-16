e2d79dafed8a24dda032ec4506763d874c01c916
Sami Nieminen <sami1.nieminen@intel.com>
2017-11-16 11:06:14 +0200

    testhardware script
    added testhardware.py for testing hardware
    currently tests cpu usage

--- cpustatus.py
+++ cpustatus.py
@@ -1,6 +1,14 @@
 import psutil
 
-status = psutil.cpu_times_percent(interval=1, percpu=False)
-print(status)
-sum = round(100 - status[3],1)
-print("Current active use of CPU: {} %".format(sum))
+#status = psutil.cpu_times_percent(interval=1, percpu=False)
+#print(status)
+#sum = round(100 - status[3],1)
+#print("Current active use of CPU: {} %".format(sum))
+
+def cpu_percentage(max_cpu):
+    status = psutil.cpu_times_percent(interval=1, percpu=False)
+    usage = round(100 - status[3],1)
+    if usage > max_cpu:
+        return "exceeded"
+    else:
+        return "ok"
--- testhardware.py
+++ testhardware.py
@@ -1,6 +1,24 @@
 import unittest
 import cpustatus
-import diskstatus
-import memorystatus
+#import diskstatus
+#import memorystatus
 
+max_cpu = 30.0
+network_name = "enp2s0"
+max_memory = 30.0
+mount1 = "/"
+mount2 = "/home"
 
+class TestHardware(unittest.TestCase):
+
+    def test_cpu_util(self):
+        self.assertEqual(cpustatus.cpu_percentage(max_cpu),"ok")
+    def test_network(self):
+        pass
+    def test_disks(self):
+        pass
+    def test_memory(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
